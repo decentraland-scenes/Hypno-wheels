9535ef2672a046cdef451d1d859c91a8dc5ad7a5
nearnshaw <nearnshaw@decentraland.org>
2019-06-20 18:05:52 -0300

    P2P

--- src/game.ts
+++ src/game.ts
@@ -1,13 +1,35 @@
 // Custom component to handle wheel spinning
 @Component('wheelSpin')
 export class WheelSpin {
+  id: number
   active: boolean = false
   speed: number = 30
   direction: Vector3 = Vector3.Up()
+  constructor(id:number){
+	this.id = id
+  }
 }
 
 // This group keeps track of all entities with a WheelSpin component
 const wheels = engine.getComponentGroup(WheelSpin)
+
+
+// a message bus to sync state for all players
+const sceneMessageBus = new MessageBus()
+
+
+/// --- Define a custom type to pass in messages ---
+type WheelState = {
+	id: number,
+	active: boolean,
+	speed: number
+  };
+
+type AllWheelsState = {wheels:[
+	WheelState]
+  };
+
+
 
 // This system carries out the rotation on each frame
 export class RotatorSystem implements ISystem {
@@ -75,8 +97,8 @@ wheel1.addComponent(spiralMaterial)
 wheel2.addComponent(spiralMaterial)
 
 // Add the custom component to the wheels
-wheel1.addComponent(new WheelSpin())
-wheel2.addComponent(new WheelSpin())
+wheel1.addComponent(new WheelSpin(1))
+wheel2.addComponent(new WheelSpin(2))
 
 // Change the direction for wheel2 (wheel1 is left with the default direction `Up`)
 wheel2.getComponent(WheelSpin).direction = Vector3.Down()
@@ -84,27 +106,59 @@ wheel2.getComponent(WheelSpin).direction = Vector3.Down()
 // Set the click behavior for the wheels
 wheel1.addComponent(
   new OnClick(e => {
-    let spin = wheel1.getComponent(WheelSpin)
-    if (!spin.active){
-      spin.active = true
-    } else {
-      spin.speed += 20
-    }
-    //log("speed: ", spin.speed)
+    emitState(1)
   })
 )
 
 wheel2.addComponent(
   new OnClick(e => {
-    let spin = wheel2.getComponent(WheelSpin)
-    if (!spin.active){
-      spin.active = true
-    } else {
-      spin.speed += 30
-    }
-    //log("speed: ", spin.speed)
+    emitState(2)
   })
 )
 
+function emitState(id: number = null){
+	
+	let wheelsList = []
+	for (let wheel of wheels.entities) {
+		let spin = wheel.getComponent(WheelSpin)
+		let wheelState: WheelState = {
+			id : spin.id,
+			active : spin.active,
+			speed : spin.speed
+		}
+		if (wheelState.id == id){
+			if (!wheelState.active){
+				wheelState.active = true
+			  } else {
+				wheelState.speed += 30
+			  }
+		}
 
+		wheelsList.push(wheelState)
+	  }
+	sceneMessageBus.emit("spinningWheels", {wheels: wheelsList})
+}
+
+sceneMessageBus.on("spinningWheels", (info:AllWheelsState) => {
+	for (let wheel of wheels.entities) {
+		let spin = wheel.getComponent(WheelSpin)
+		for (let w of info.wheels) {
+			if (spin.id == w.id){
+				spin.active = w.active
+				spin.speed = w.speed
+			}
+		}
+	}
+  });
+
+
+
+  // To get the initial state of the scene when joining
+  sceneMessageBus.emit("getWheelState",{})
+  
+  // To return the initial state of the scene to new players
+  sceneMessageBus.on("getWheelState", () => {
+	emitState()
+  });
+  
 
